737a357b25113de459756fb651d2282d0d13f333
zakzaks <ariefn82@gmail.com>
2025-08-22 20:46:39 +0700

    nested page and layout

---
+++ app/admin/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminPage() {
+	return (
+		<>
+			<h1 className="text-3xl font-bold mb-4">Admin Page</h1>
+			<p>This is the admin page content.</p>
+		</>
+	);
+}
---
+++ app/admin/product/page.tsx
@@ -0,0 +1,8 @@
+export default function ProductPage() {
+	return (
+		<>
+			<h1 className="text-3xl font-bold mb-4">Product Page</h1>
+			<p>This is the product page content.</p>
+		</>
+	);
+}
